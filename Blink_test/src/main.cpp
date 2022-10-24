8e1d6b8d4137597f480af27a180df03aa0fd5e82
1998-felix <felix.gateru@gmail.com>
2022-10-24 20:52:55 +0300

    refactoring

--- Blink_test/src/main.cpp
+++ Blink_test/src/main.cpp
@@ -1,18 +1,16 @@
 // include
 #include <Arduino.h>
 
-
 // define
 #define BLINK_LED 13
 
-
-
-void setup() {
-  pinMode(BLINK_LED,OUTPUT);
-
+void setup()
+{
+  pinMode(BLINK_LED, OUTPUT);
 }
 
-void loop() {
- digitalWrite(BLINK_LED,!digitalRead(BLINK_LED));
- delay(500);
+void loop()
+{
+  digitalWrite(BLINK_LED, !digitalRead(BLINK_LED));
+  delay(500);
 }
